b883fb1c0808c58099d27c89674843e7fdb64dd8
CybeatB <cybeatb@gmail.com>
2015-08-10 13:54:59 +0930

    Improving Process Comparator

--- Process.java
+++ Process.java
@@ -1,19 +1,35 @@
 public class Process {
   // Constructor
-  public Process() {}
+  public Process(int time, String act, String type, int seats, String id) {
+    this._time = time;
+    this._action = act;
+    this._type = type;
+    this._seats = seats;
+    this._id = id;
+  }
 
   // Methods
   public int compareTo(Process proc) {
     // this <(-1) =(0) >(+1)
-    if (!this._type.equals(proc.type())) {
-      return this._type.equals("C") ? -1 : 1;
+    if (!this._action.equals(proc.action())) {
+      return this._action.equals("C") ? -1 : 1;
     }
     if (this._time != proc.time()) {
       return this._time - proc.time();
     }
+    if (!this._type.equals(proc.type())) {
+      // F > B > E
+    }
+    if (this._seats != proc.seats()) {
+      return this._seats - proc.seats();
+    }
+    return this._id.compareTo(proc.id());
   }
   public int time() {
     return this._time;
+  }
+  public String action() {
+    return this._action;
   }
   public String type() {
     return this._type;
@@ -24,9 +40,10 @@ public class Process {
   public String agentID() {
     return this._id;
   }
-//TODO: differentiate between action & type
+
   // Variables
   private int _time;
+  private String _action;
   private String _type;
   private int _seats;
   private String _id;
